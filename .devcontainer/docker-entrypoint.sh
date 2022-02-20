910f81364b02decb107b86db71ffc3f1e022b8a8
Raki Rahman <mdrrahman@microsoft.com>
2022-02-20 08:54:25 -0500

    Configured exercism

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello from our entrypoint!"
+echo "Configuring wakatime..."
+printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg;
+
+echo "Configuring exercism..."
+exercism configure --token=$EXERCISM_TOKEN -w /workspace;
+mkdir -p /workspace/$TRACK;
+cd /workspace/$TRACK;
 
 exec "$@"
