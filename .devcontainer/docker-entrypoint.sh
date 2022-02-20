65bf576b1cccd77f9fc8f7776d3d93d5fac6ca3a
Raki Rahman <mdrrahman@microsoft.com>
2022-02-20 09:27:35 -0500

    Added progress

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -4,8 +4,6 @@ echo "Configuring wakatime..."
 printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg;
 
 echo "Configuring exercism..."
-exercism configure --token=$EXERCISM_TOKEN -w /workspace;
-mkdir -p /workspace/$TRACK;
-cd /workspace/$TRACK;
+exercism configure --token=$EXERCISM_TOKEN -w /workspaces/exercism_dotnet;
 
 exec "$@"
