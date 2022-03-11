05132c933bd2d21560444ead7186b2980c112a6a
Raki Rahman <mdrrahman@microsoft.com>
2022-03-10 21:37:10 -0500

    Completed namespaces

---
+++ red-vs-blue-darwin-style/RedVsBlueDarwinStyle.cs
@@ -0,0 +1,55 @@
+namespace RedRemoteControlCarTeam
+{
+    public class RemoteControlCar
+    {
+        public RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry, RunningGear runningGear)
+        {
+        }
+    }
+    public class RunningGear {}
+    public class Telemetry {}
+    public class Chassis {}
+    public class Motor {}
+}
+
+namespace BlueRemoteControlCarTeam
+{
+    public class RemoteControlCar
+    {
+        public RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry)
+        {}
+    }
+    public class Telemetry
+    {}
+    public class Chassis
+    {}
+    public class Motor
+    {}
+}
+
+namespace Combined
+{
+    using Red = RedRemoteControlCarTeam;
+    using Blue = BlueRemoteControlCarTeam;
+    public static class CarBuilder
+    {
+        public static Red.RemoteControlCar BuildRed()
+        {
+            return new Red.RemoteControlCar(
+                new Red.Motor(),
+                new Red.Chassis(),
+                new Red.Telemetry(),
+                new Red.RunningGear()
+            );
+        }
+
+        public static Blue.RemoteControlCar BuildBlue()
+        {
+            return new Blue.RemoteControlCar(
+                new Blue.Motor(),
+                new Blue.Chassis(),
+                new Blue.Telemetry()
+            );
+        }
+    }
+}
