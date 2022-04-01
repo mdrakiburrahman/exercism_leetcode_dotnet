28bee4a295425f33922535c673bd69feb6143684
Raki Rahman <mdrrahman@microsoft.com>
2022-03-31 22:02:15 -0400

    Prime Factors

--- prime-factors/PrimeFactors.cs
+++ prime-factors/PrimeFactors.cs
@@ -1,29 +1,20 @@
-using System;
 using System.Collections.Generic;
 
-
-var p = PrimeFactors.Factors(93819012551L);
-// Print the array
-Console.WriteLine(string.Join(",", p));
-
-public static class PrimeFactors
+public class PrimeFactors
 {
-    public static List<long> appendFactors(List<long> factors, long number, long divisor)
-    {
-        if (number == 1) // No longer divisible by prime
-            return factors;
-        if (number % divisor == 0) // Valid divisor
-        {   
-            factors.Add(divisor);
-            // Try again with the same divisor recursively
-            return appendFactors(factors, number/divisor, divisor);
-        }
-        else // Divisor did not match
-            return appendFactors(factors, number, divisor+1); // Increment
-    }
     public static long[] Factors(long number)
     {
-        List<long> factors = new List<long>();
-        return appendFactors(factors, number, 2).ToArray(); // Start at 2
+        var primes = new List<long>(); // List of Primae divisors
+        int divisor = 2;
+        while (number > 1) // Stop looping when remaining number is 1
+        {
+            while (number % divisor == 0) // If next number is a divisor, add it
+            {
+                primes.Add(divisor);
+                number = number/divisor;
+            }
+            divisor++; // Increment divisor
+        }
+        return primes.ToArray();
     }
 }
