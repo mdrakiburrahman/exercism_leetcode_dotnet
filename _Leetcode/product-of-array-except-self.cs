7405f1cdc34955ff4a254d563bf43120fe6cacff
Raki Rahman <mdrrahman@microsoft.com>
2022-04-03 07:09:28 -0400

    Improved to O(1)

--- _Leetcode/product-of-array-except-self.cs
+++ _Leetcode/product-of-array-except-self.cs
@@ -1,37 +1,21 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
         var answer = new int[nums.Length]; // [0, ..., 0]
-        var numZeros = 0; // Counter for zeros
-        int totalProduct = 1;
-        // Loop over to get products, ignore zero - deal seperately
-        foreach(int n in nums)
+        
+        // Calculate left products
+        answer[0] = 1; // Nothing to the left
+        
+        for(int i = 1; i < nums.Length; i++) // Left to right
         {
-            if (n != 0)
-            {
-                totalProduct *= n;
-            } else {
-                numZeros++;
-            }
-        }
-        // Special case of multiple zeros
-        if (numZeros >= 2)
+            answer[i] = answer[i-1] * nums[i-1]; 
+        }        
+        
+        var R = 1; // Running product from right
+        
+        for (int i = nums.Length-1; i >= 0; i--) // Right to left
         {
-            return answer; // [0, ..., 0]
-        }
-        // Fill up answer, numZeros <= 1 going forward;
-        for(int i = 0; i < nums.Length; i++)
-        {
-            if (numZeros != 0) // Have that one zero
-            {
-               if (nums[i] == 0) // That one zero
-                {
-                    answer[i] = totalProduct; // Product as is
-                } 
-                // Else, keep everything as zero
-            } else { 
-                // No zeros in our array, deal with as usual
-                answer[i] = (int)((double)totalProduct * System.Math.Pow(nums[i], -1));
-            }
+            answer[i] = answer[i] * R;
+            R = nums[i] * R;
         }
         return answer;
     }
