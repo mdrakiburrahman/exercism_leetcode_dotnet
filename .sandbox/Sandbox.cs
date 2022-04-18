fd847dc039e65d89dcc4a483d1eda9f624b61369
Raki Rahman <mdrrahman@microsoft.com>
2022-04-18 09:24:34 -0400

    Need to meet a few other cases

--- .sandbox/Sandbox.cs
+++ .sandbox/Sandbox.cs
@@ -1,29 +1,45 @@
 using System;
 using System.Linq;
 
-char[] s = { 't','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e' };
+char[] s1 = { 't','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e' };
+Solution sol = new Solution();
+sol.ReverseWords(s1);
 
-Solution.ReverseWords(s);
+char[] s2 = { 'a' };
+sol.ReverseWords(s2);
 
-public static class Solution {
-    public static void ReverseWords(char[] s) {
+char[] s3 = { 'a',' ','b',' ','c',' ','d',' ','e' };
+sol.ReverseWords(s3);
+
+char[] s4 = { 'h','i'};
+sol.ReverseWords(s4);
+
+public class Solution {
+    public void ReverseWords(char[] s) {
+        if (s == null || s.Length == 0 || s.Length == 1 || !s.Contains(' ')) return;
+
         int left_ptr = 0, right_ptr = (s.Length - 1);
         string buffer = ""; // 0. Start buffer as empty
         
         
-        while (left_ptr < right_ptr) // Loop until left_ptr >= right_ptr
+        while (left_ptr <= right_ptr) // Loop until left_ptr >= right_ptr
         {
             // 1. Start from the right, find the first word - including the space, set right_ptr
-            (int, string) tuple = GetRightWord(s, right_ptr);
-            
-            right_ptr = tuple.Item1 - 1; // One over from _
-            string new_word = tuple.Item2 + " ";
+            (int, string) tuple1 = GetRightWord(s, right_ptr);
+            string new_word = tuple1.Item2 + " ";
             
             // 2. For the number of digits we found in new_word, copy to a 'buffer' from front
             // 3. Copy the new_word to front, set left_ptr
-            tuple = CopyRightWord(s, new_word, left_ptr);
-            left_ptr = tuple.Item1;
-            buffer += tuple.Item2;
+            (int, string) tuple2 = CopyRightWord(s, new_word, left_ptr);
+            buffer += tuple2.Item2;
+
+            if (left_ptr != right_ptr) // If pointers are same, we want to start copying from here
+            {
+                right_ptr = tuple1.Item1 - 1; // One over from _
+                left_ptr = tuple2.Item1;
+            } else {
+                break;
+            }
         }
 
         // From left_ptr -> Split buffer by space -> Copy per word into s -> return
@@ -35,10 +51,9 @@ public static class Solution {
             s[i] = words[i - left_ptr];
         }
 
-        Console.WriteLine(String.Join("", s));
     }
     
-    public static (int, string) CopyRightWord(char[] s, string new_word, int left_ptr)
+    public (int, string) CopyRightWord(char[] s, string new_word, int left_ptr)
     {
         string buffer = "";
         
@@ -56,7 +71,7 @@ public static class Solution {
         return(left_ptr + new_word.Length, buffer);
     }
     
-    public static (int, string) GetRightWord(char[] s, int right_ptr)
+    public (int, string) GetRightWord(char[] s, int right_ptr)
     {
         string word = "";
         
